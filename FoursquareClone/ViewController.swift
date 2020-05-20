84a93be630b795f7b839eae3379c71b5a4c68f15
Yunus MAKAN <makanyunus@gmail.com>
2020-05-20 06:46:15 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by MAKAN on 20.05.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
