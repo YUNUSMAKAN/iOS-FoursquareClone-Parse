e47f7f6938e7ac2b8cd1b1cbf590e8b586b8e059
Yunus MAKAN <makanyunus@gmail.com>
2020-05-27 03:18:03 +0300

    Add Files

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Server baglati islemi
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "bavoEx5TG3nMBWr9lliSN7rDvrcHvgQ7hI4htlpt"
+            ParseMutableClientConfiguration.clientKey = "4FmAiNK86aXmnkztmP3N9xleXrrIB6uY4rtC5rUF"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+            
+        }
+            
+            
+        Parse.initialize(with: configuration)
+        
+        
+        
+        
         return true
     }
 
--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addButtonClicked(){
+        //segue
     }
-    */
 
+    @objc func logoutButtonClicked(){
+        
+        PFUser.logOutInBackground { (error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK!", style: UIAlertAction.Style.default , handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil )
+                
+            }else {
+                self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
+            }
+        }
+        
+        
+    }
 }
