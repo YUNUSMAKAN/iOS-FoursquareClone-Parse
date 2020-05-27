df0f27cc869b5684d674a8ecbc63f3c57a851323
Yunus MAKAN <makanyunus@gmail.com>
2020-05-27 03:15:19 +0300

    first commit

---
+++ FoursquareClone/PlacesVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlacesVC.swift
+//  FoursquareClone
+//
+//  Created by MAKAN on 27.05.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class PlacesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoursquareClone/SignUpVC.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by MAKAN on 20.05.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpVC: UIViewController {
+
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        /*
+        //PARSE VERI KAYDETME ISLEMI
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
+        parseObject.saveInBackground { (success, error) in
+            if error != nil{
+                print(error?.localizedDescription)
+            }else {
+                print("uploaded")
+            }
+        
+        }
+         
+        //VERI CEKME ISLEMI
+        let query = PFQuery(className: "Fruits")
+        //query.whereKey("name", equalTo: "Apple") //filtreleme islemi.ismi elma olanlari getir dedik.
+        query.whereKey("calories", greaterThan: 120) //kalorisi 120 den buyuk olanlari getir dedik.
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil{
+                print(error?.localizedDescription)
+                
+            }else{
+                print(objects)
+            }
+            
+        }
+        */
+      
+        
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if userNameText.text != "" && passwordText.text != "" {
+            
+            PFUser.logInWithUsername(inBackground: userNameText.text!, password: passwordText.text!) { (user, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error" )
+                }else {
+                    //Segue
+                    self.performSegue(withIdentifier: "toPlacesVC", sender: nil)
+                }
+            }
+            
+            
+        }else {
+            makeAlert(titleInput: "Error", messageInput: "Username / Password ??")
+        }
+        
+    }
+    
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if userNameText.text != "" && passwordText.text != "" {
+            
+            let user = PFUser()
+            user.username = userNameText.text!
+            user.password = passwordText.text!
+            
+            user.signUpInBackground { (success, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error!!!")
+                } else {
+                    //Segue
+                    self.performSegue(withIdentifier: "toPlacesVC", sender: nil)
+                    
+                }
+            }
+            
+        }else {
+            makeAlert(titleInput: "Error", messageInput: "Username/ Password ??")
+            
+        }
+    }
+    
+    
+        
+    func makeAlert(titleInput: String , messageInput: String){
+        
+            let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        
+        
+    
+    
+}
+
