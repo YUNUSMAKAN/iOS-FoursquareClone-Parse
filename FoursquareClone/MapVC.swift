58d6922983706612d83bcab9e3710c544d2985d2
Yunus MAKAN <makanyunus@gmail.com>
2020-05-30 04:02:48 +0300

    Getting the user's location!

--- FoursquareClone/MapVC.swift
+++ FoursquareClone/MapVC.swift
@@ -9,16 +9,38 @@
 import UIKit
 import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(title: "Save", style: UIBarButtonItem.Style.plain, target: self, action: #selector(saveButtomClicked))
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "< Back", style: UIBarButtonItem.Style.plain, target: self, action: #selector(backButtomClicked))
         
+        //Haritada yer gosterme islemi.
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //yerimizi ne kadar dogru bulucagini belirtiyoruz.
+        locationManager.requestWhenInUseAuthorization()  //sadece kullandigim zaman gostermen yeterli dedik.
+        locationManager.startUpdatingLocation() //kullanicinin bulundugu yeri guncelleriz.
+        
+        
     }
+    //KULLANICININ YERI KAYDEDILDIKTEN SONRA NE OLUCAK?
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //locationManager.stopUpdatingLocation() //Bu sekilde sadece lokasyon bir defa guncellenir her acildiginda tekrar guncellenmez.
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035) //Ne kadar kucuk yaparsak degerleri o derece zoom edicek.
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    
     
     @objc func saveButtomClicked(){
         //PARSE SAVE
